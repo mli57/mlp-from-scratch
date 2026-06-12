1c3858930b19ab43fa6bf77ffc453487ae49f9fb
mli57 <maxwell.l4991@gmail.com>
2026-06-12 16:18:10 +0800

    Added implementation for layers.py

---
+++ src/layers.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Linear:
+    def __init__(self, in_features, out_features):
+        # initialization, same as PyTorch's default for ReLU networks
+        self.W = np.random.randn(in_features, out_features) * np.sqrt(2.0 / in_features)
+        self.b = np.zeros(out_features)
+        self.grad_W = None
+        self.grad_b = None
+
+    def forward(self, x):
+        self.x = x  # save input for backprop
+        return x @ self.W + self.b  # same as nn.Linear
+
+    def backward(self, grad):
+        # grad is dLoss/d(output of this layer)
+        self.grad_W = self.x.T @ grad   # dLoss/dW
+        self.grad_b = grad.sum(axis=0)  # dLoss/db
+        return grad @ self.W.T          # dLoss/d(input), passed to previous layer
